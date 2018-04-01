0d0a4959b664053378327c4eed74dbd30f62645a
Raphael DeFranco <>
2018-03-31 22:33:29 -0700

    Initial basic functionality.

---
+++ db.js
@@ -0,0 +1,38 @@
+const { Pool, Client } = require('pg')
+const client = new Client({
+  user: 'cordboard',
+  host: 'localhost',
+  database: 'chat',
+  password: '',
+  port: 5432,
+})
+client.connect()
+
+module.exports = {
+	register: function(data, public_id, private_id) {
+		client.query('INSERT INTO chat_user(public_data, public_id, private_id) VALUES($1, $2, $3)', [data, public_id, private_id], function (err, res) {
+			if (err) throw err
+		});
+	},
+	getData: function(public_id, callback) {
+		client.query('SELECT data FROM chat_user WHERE public_id = $1', [public_id], function(err, res) {
+			if (err) throw err
+			if (res.rows.length == 1) {
+				callback(res.rows[0].data);
+			}
+		});
+	},
+	goOnline: function(private_id, socket_id, callback) {
+		client.query('UPDATE chat_user SET socket_id=$1 WHERE private_id=$2', [socket_id, private_id], function(err, res) {
+			if (err) throw err
+			if (res.rows.length == 1) {
+				callback(true);
+			}
+		});
+	},
+	goOffline: function(socket_id) {
+		client.query('UPDATE chat_user SET socket_id=NULL WHERE socket_id=$1', [socket_id], function(err, res) {
+			if (err) throw err
+		});
+	}
+}
---
+++ index.js
@@ -0,0 +1,58 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var db = require('./db')
+const uuidv1 = require('uuid/v1');
+
+
+app.get('/', function(req, res){
+  console.log('requested root');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  //get a unique id that can be used as an address
+  //to recieve messages. Save some data (such as a public key)
+  //that will be passed to anyone who requests it based on the uuid
+  //through the dataForId route.
+  //data: {obj} -> String
+  socket.on('register', function(data) {
+    var public = uuidv1();
+    var private = uuidv1();
+    db.register(data, public, private);
+    socket.emit('registerComplete', {public: public, private: private});
+  });
+
+  //post a private_id to tell the server to associate your current
+  //socket id with your public id so that you can be sent messages.
+  socket.on('goOnline', function(private_id) {
+    db.goOnline(private_id, socket.id, function(success) {
+      socket.emit('goOnline', success);
+    });
+  });
+
+  socket.on('disconnect', function(reason) {
+    db.goOffline(socket.id);
+  });
+  
+  //Get the associated data for a given address (id).
+  //uuid: String
+  socket.on('dataForId', function(uuid) {
+    db.getData(uuid, function(data) {
+      console.log(data);
+      socket.emit('dataForId', data);
+    });
+  });
+  
+  //Send a message to one or more registered ids
+  //data should be of format [{uuid: String, message: String}, ...]
+  socket.on('sendMessage', function(data){
+    console.log('msg rcvd');
+    console.log(data);
+    socket.emit('sendMessage', data);
+  });
+});
+
+http.listen(3306, function(){
+  console.log('listening on *:3306');
+});
