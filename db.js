607969999f870ac1c62a348067afb4c311157c00
Raphael DeFranco <>
2018-04-06 22:09:31 -0700

    Get stuff from the correct table.

--- db.js
+++ db.js
@@ -12,12 +12,12 @@ client.connect()
 
 module.exports = {
 	register: function(data, public_id, private_id) {
-		client.query('INSERT INTO chat_user(public_data, public_id, private_id) VALUES($1, $2, $3)', [data, public_id, private_id], function (err, res) {
+		client.query('INSERT INTO cordboard(public_data, public_id, private_id) VALUES($1, $2, $3)', [data, public_id, private_id], function (err, res) {
 			if (err) throw err
 		});
 	},
 	getData: function(public_id, callback) {
-		client.query('SELECT data FROM chat_user WHERE public_id = $1', [public_id], function(err, res) {
+		client.query('SELECT data FROM cordboard WHERE public_id = $1', [public_id], function(err, res) {
 			if (err) throw err
 			if (res.rows.length == 1) {
 				callback(res.rows[0].data);
@@ -25,7 +25,7 @@ module.exports = {
 		});
 	},
 	goOnline: function(private_id, socket_id, callback) {
-		client.query('UPDATE chat_user SET socket_id=$1 WHERE private_id=$2', [socket_id, private_id], function(err, res) {
+		client.query('UPDATE cordboard SET socket_id=$1 WHERE private_id=$2', [socket_id, private_id], function(err, res) {
 			if (err) throw err
 			if (res.rows.length == 1) {
 				callback(true);
@@ -33,7 +33,7 @@ module.exports = {
 		});
 	},
 	goOffline: function(socket_id) {
-		client.query('UPDATE chat_user SET socket_id=NULL WHERE socket_id=$1', [socket_id], function(err, res) {
+		client.query('UPDATE cordboard SET socket_id=NULL WHERE socket_id=$1', [socket_id], function(err, res) {
 			if (err) throw err
 		});
 	}
